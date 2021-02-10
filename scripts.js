d6e4fd5c83e67b61ed508eacdaf29d428765245f
Diego Ferreira <diegoferreira1964@gmail.com>
2021-02-10 19:45:41 -0300

    Formatting data for Brazilian currency and starting to interact with HTML

--- scripts.js
+++ scripts.js
@@ -61,9 +61,13 @@ const DOM = {
         DOM.transactionsContainer.appendChild(tr)
     },
     innerHTMLTransaction(transaction) {
+      const CSSclass = transaction.amount > 0 ? "income" : "expense";
+
+      const amount = Utils.formatCurrency(transaction.amount)
+
       const html = `
                 <td class="description">${transaction.description}</td>
-                <td class="expense">${transaction.amount}</td>
+                <td class="${CSSclass}">${amount}</td>
                 <td class="date">${transaction.date}</td>
                 <td>
                      <img src="./assets/minus.svg" alt="Remover transação"> 
@@ -71,9 +75,39 @@ const DOM = {
         `
 
         return html
+    },
+    updateBalance() {
+        document
+            .getElementById('incomeDisplay')
+            .innerHTML = "Soma das entradas";
+        document
+            .getElementById('expenseDisplay')
+            .innerHTML = "Soma das saidas";
+        document
+            .getElementById('totalDisplay')
+            .innerHTML = "Total"
+    }
+}
+
+const Utils = { 
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : "";
+        
+        value = String(value).replace(/\D/g, "");
+        
+        value = Number(value) / 100;
+
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        }) 
+         
+        return signal + value;
     }
 }
 
 transactions.forEach(function(transaction) {
     DOM.addTransaction(transaction)
 })
+
+DOM.updateBalance();
